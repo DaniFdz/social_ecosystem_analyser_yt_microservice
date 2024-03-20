806c9feb5b4370cc164ec614206386cfe50a9e22
DaniFdz <danifernandezzzzzz@gmail.com>
2024-03-20 18:37:41 +0000

    Fixed file not found error

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-/root/.local/bin/poetry run python3 -m src.main.python.SocialEcosystemAnalyser.social_ecosystem_analyser_youtube > /root/.logs/yt_service.log 2> /root/.logs/yt_service_error.log
+/root/.local/bin/poetry run python3 -m src.main.python.SocialEcosystemAnalyser.social_ecosystem_analyser > /root/.logs/yt_service.log 2> /root/.logs/yt_service_error.log
 /root/.local/bin/poetry run python3 -m src.main.python.SocialEcosystemAnalyser.social_ecosystem_analyser_examination > /root/.logs/vt_service.log 2> /root/.logs/vt_service_error.log
